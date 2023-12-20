bb54c0106ce76bb8efcc1f7e065b855d30e49c67
devEMEL <dominichenry882@gmail.com>
2023-12-20 21:00:56 +0100

    first commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/main.css'
-import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
